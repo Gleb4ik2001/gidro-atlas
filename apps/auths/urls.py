782740ab7cd348d624c5bd13a3e79f2cc12b4cf4
Gleb4ik2001 <g.kalashnikovvv@gmail.com>
2025-12-07 03:44:41 +0500

    feat: added logout view

--- apps/auths/urls.py
+++ apps/auths/urls.py
@@ -4,11 +4,12 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
-from .views import login_view
+from .views import login_view, logout_view
 
 
 urlpatterns = [
     path("login/", login_view),
+    path("logout/", logout_view),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- apps/auths/views.py
+++ apps/auths/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate
 
+
 @api_view(["POST"])
 def login_view(request):
     login = request.data.get("login")
@@ -23,3 +24,14 @@ def login_view(request):
             "role": user.role,
         }
     })
+
+
+@api_view(["POST"])
+def logout_view(request):
+    try:
+        refresh_token = request.data["refresh"]
+        token = RefreshToken(refresh_token)
+        token.blacklist()
+        return Response({"detail": "Logout successful"}, status=205)
+    except Exception as e:
+        return Response({"detail": "Invalid token"}, status=400)
