ad6411bb48ceda8a223caf892c3faa2045137e21
Gleb4ik2001 <g.kalashnikovvv@gmail.com>
2025-12-07 15:44:18 +0500

    feat: added ML-model and changed access token lifetime to 20 minutes

---
+++ apps/ml/predictor.py
@@ -0,0 +1,12 @@
+import os
+import pickle
+
+BASE_DIR = os.path.dirname(__file__)
+MODEL_PATH = os.path.join(BASE_DIR, "model.pkl")
+
+with open(MODEL_PATH, "rb") as f:
+    MODEL = pickle.load(f)
+
+def predict_attention_probability(technical_condition, passport_age):
+    features = [[technical_condition, passport_age]]
+    return float(MODEL.predict_proba(features)[0][1])
